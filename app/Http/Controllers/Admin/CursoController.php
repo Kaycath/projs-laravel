31f0a98b962208050372461a597e01d154a10d02
Kaique Almeida Real Rodrigues <ra2457024@MacBook-Air-de-MAC413.local>
2026-05-16 07:34:54 -0300

    Concluindo a Aula 2 do CRUD

--- app/Http/Controllers/Admin/CursoController.php
+++ app/Http/Controllers/Admin/CursoController.php
@@ -11,4 +11,64 @@ class CursoController extends Controller
         $rows = Curso::all();
         return view('admin.cursos.index', compact('rows'));
     }
+
+    public function adicionar() {
+        return view('admin.cursos.adicionar');
+    }
+
+    public function editar($id) {
+        $linha = Curso::find($id); // carrega o registro
+        return view('admin.cursos.editar', compact('linha'));
+    }
+
+    public function excluir($id) {
+        Curso::find($id)->delete(); // deleta o registro
+        return redirect()->route('admin.cursos');
+    }
+
+    public function salvar(Request $req) {
+        $dados = $req->all();
+
+        if(isset($dados['publicado'])){
+            $dados['publicado'] = 'sim';
+        } else {
+            $dados['publicado'] = 'nao';
+        }
+
+        if($req->hasFile('arquivo')){
+            $imagem = $req->file('arquivo');
+            $num = rand(1111,9999);
+            $dir = "img/cursos/";
+            $ex = $imagem->guessClientExtension();
+            $nomeImagem = "imagem_".$num.".".$ex;
+            $imagem->move($dir,$nomeImagem);
+            $dados['imagem'] = $dir."/".$nomeImagem; 
+        }
+
+        Curso::create($dados);
+        return redirect()->route('admin.cursos');
+    }
+
+    public function atualizar(Request $req, $id) {
+        $dados = $req->all();
+
+        if(isset($dados['publicado'])){
+            $dados['publicado'] = 'sim';
+        } else {
+            $dados['publicado'] = 'nao';
+        }
+
+        if($req->hasFile('arquivo')){
+            $imagem = $req->file('arquivo');
+            $num = rand(1111,9999);
+            $dir = "img/cursos/";
+            $ex = $imagem->guessClientExtension();
+            $nomeImagem = "imagem_".$num.".".$ex;
+            $imagem->move($dir,$nomeImagem);
+            $dados['imagem'] = $dir."/".$nomeImagem;
+        }
+
+        Curso::find($id)->update($dados);
+        return redirect()->route('admin.cursos'); 
+    }
 }
